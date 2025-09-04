abfadaa33026c6bc0bb7a336cb4f9c3205eb70ef
bru1f0rc3 <wint1841@inbox.ru>
2025-09-04 13:19:30 +0300

    ready

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Выберите: \n" +
-                    "1 - Первое задание\n" +
-                    "2 - Второе задание\n" +
-                    "3 - Третье задание\n\n" +
-                    "0 - Выход из программы");
-            Scanner in = new Scanner(System.in);
-            int select_task = in.nextInt();
-
-            switch (select_task) {
-                case 1:
-                    First_Task.converterCurrency();
-                    break;
-                case 2:
-                    Second_Task.printWeatherAdvice();
-                    break;
-                case 3:
-                    Third_Task.recommendBreakfast();
-                    break;
-                case 0:
-                    return;
-                default:
-                    System.out.println("Возможно вы ввели не ту цифру...");
-            }
-        }
+        First_Task.converterCurrency();
+        Second_Task.printWeatherAdvice();
+        Third_Task.recommendBreakfast();
     }
 }
